4bab360aa6668f24062456cb59985107ec1219f2
Julia McGee <JuliaMcGee@Kenneths-MacBook-Pro.local>
2018-01-25 16:49:17 -0500

    fixed image issue

--- server.js
+++ server.js
@@ -6,6 +6,8 @@ var PORT = 8080;
 
 require("./app/routing/html-routes")(app);
 
+// app.use(express.static('view'))
+
 app.listen(PORT, function(){
     console.log("Server listening on: http://localhost:" + PORT)
 });
